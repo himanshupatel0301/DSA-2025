d72e2f964c480d2a4893a4b437970ca866daf1ab
himanshupatel0301 <71480959+himanshupatel0301@users.noreply.github.com>
2025-01-12 08:42:41 +0000

    Assignment1

--- Assignment 1/butterfly.cpp
+++ Assignment 1/butterfly.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int main()
 {
     int n;
-    cin>>n;
+    cout<<"Enter the number"<<endl;
+        cin>>n;
     for(int i =0 ;i<2*n;i++)
     {
         int cond = i<n ? i:n+(n-i-1);
-        int space_count = i<n? 2*(n-cond-1):i-cond-i;
+        int space_count = i<n ? 2*(n-cond-1):i-cond-1;
         for(int j = 0;j<2*n;j++)
         {
             if(j<=cond)
@@ -25,7 +26,7 @@ int main()
                     cout<<"*";
                 }
             }
-                                cout<<endl;
+                     cout<<endl;
 
 
         }
